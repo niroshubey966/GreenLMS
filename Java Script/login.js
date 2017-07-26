bcd1a04fc4564dc4d055d140bcd2f605dc02455f
Nirosh Ubeysinghe <niroshubey966@gmail.com>
2017-07-26 12:47:05 +0530

    I have changed the file paths of captcha images.

--- Java Script/login.js
+++ Java Script/login.js
@@ -6,10 +6,57 @@ function choosePic() {
     }
 function test(){
     if(
-        document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha1.png"  && document.getElementById("text").value=="0034523" || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha2.jpg"  && document.getElementById("text").value=="polish" || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha3.png" && document.getElementById("text").value=="4359" || 
-        document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha4.gif" && document.getElementById("text").value=="W6 8HP"  || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha5.gif" && document.getElementById("text").value=="PQJRYD"  || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha6.png" && document.getElementById("text").value=="ABCDeF"  || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha7.jpg" && document.getElementById("text").value=="CAPTCHA"  || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha8.png" && document.getElementById("text").value=="3sAE hvo7sE"  || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha9.png" && document.getElementById("text").value=="profit"  || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha10.png" && document.getElementById("text").value=="finans"  || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha11.jpg" && document.getElementById("text").value=="caPtchA"  || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha12.png" && document.getElementById("text").value=="Viearer"  || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha13.png" && document.getElementById("text").value=="JeMH5LP4"  || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha14.png" && document.getElementById("text").value=="Viearer"  || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha15.png" && document.getElementById("text").value=="4359"   || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha16.jpeg" && document.getElementById("text").value=="following finding"  || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha17.jpg" && document.getElementById("text").value=="4076"   || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha18.jpg" && document.getElementById("text").value=="2078"   || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha19.jpg" && document.getElementById("text").value=="2502"   || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha20.jpg" && document.getElementById("text").value=="103"  || 
-        document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha21.jpg" && document.getElementById("text").value=="6325"   || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha22.jpg" && document.getElementById("text").value=="2438"   || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha23.jpg" && document.getElementById("text").value=="47"   ||  
-        document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha24.jpg" && document.getElementById("text").value=="1541"   || document.getElementById("myPicture").src=="file:///C:/Users/user/Desktop/GreenLMS-master/captcha25.jpg" && document.getElementById("text").value=="2599" )
+        document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha1.png"  &&
+        document.getElementById("text").value=="0034523" 
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha2.jpg"  &&
+        document.getElementById("text").value=="polish" 
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha3.png" && 
+        document.getElementById("text").value=="4359" 
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha4.gif" && 
+        document.getElementById("text").value=="W6 8HP"  
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha5.gif" &&
+        document.getElementById("text").value=="PQJRYD"  
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha6.png" &&
+        document.getElementById("text").value=="ABCDeF"  
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha7.jpg" &&
+        document.getElementById("text").value=="CAPTCHA"  
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha8.png" &&
+        document.getElementById("text").value=="3sAE hvo7sE"  
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha9.png" &&
+        document.getElementById("text").value=="profit"  
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha10.png" &&
+        document.getElementById("text").value=="finans"  
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha11.jpg" &&
+        document.getElementById("text").value=="caPtchA"  
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha12.png" &&
+        document.getElementById("text").value=="Viearer"  
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha13.png" &&
+        document.getElementById("text").value=="JeMH5LP4"  
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha14.png" &&
+        document.getElementById("text").value=="Viearer"  
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha15.png" &&
+        document.getElementById("text").value=="4359"   
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha16.jpeg" &&
+        document.getElementById("text").value=="following finding"  
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha17.jpg" &&
+        document.getElementById("text").value=="4076"   
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha18.jpg" &&
+        document.getElementById("text").value=="2078"   
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha19.jpg" &&
+        document.getElementById("text").value=="2502"   
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha20.jpg" &&
+        document.getElementById("text").value=="103"  
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha21.jpg" &&
+        document.getElementById("text").value=="6325"   
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha22.jpg" &&
+        document.getElementById("text").value=="2438"   
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha23.jpg" && 
+        document.getElementById("text").value=="47"   
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha24.jpg" &&
+        document.getElementById("text").value=="1541"   
+        || document.getElementById("myPicture").src=="http://localhost/GreenLMS-master/captcha25.jpg" &&
+        document.getElementById("text").value=="2599" 
+    )
     {
       alert("You are not a robot");
       document.getElementById('id01').style.display='none';

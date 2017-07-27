9973bcc632110f158442a819af0a71cfae94b987
Thisarani Malawaraarachchi <thish1994mm@gmail.com>
2017-07-27 12:33:59 +0530

    I have seperated the js part

--- Java Script/engineering.js
+++ Java Script/engineering.js
@@ -12,3 +12,19 @@ function carousel() {
     x[myIndex-1].style.display = "block";  
     setTimeout(carousel, 2000); // Change image every 2 seconds
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("calender");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
